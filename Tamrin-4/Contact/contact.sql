898f6a52d3a9309ef9ebaef4e2397cfe861745a5
Alireza Mehrabi <alirezamehrabie@gmail.com>
2023-04-15 01:14:41 +0330

    Create new table Contact
    Create new table Contact (Update)

---
+++ Tamrin-4/Contact/contact.sql
@@ -0,0 +1,9 @@
+CREATE TABLE contactus (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(110) NOT NULL UNIQUE,
+  message TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  status ENUM('new', 'in_progress', 'completed') NOT NULL DEFAULT 'new',
+  PRIMARY KEY (id)
+);
