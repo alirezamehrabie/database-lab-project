02d4190215b3f540e81ebf4105b31ebbe3771102
Alireza Mehrabi <alirezamehrabie@gmail.com>
2023-04-28 16:09:11 +0330

    Select owner (Ali)
    Select owner (Ali) whose name includes Ali Or Mohammad
    
    دریافت نام مالکینی که داخل نام آنها از علی یا محمد استفاده شده است

---
+++ Tamrin-11/Owner Ali.sql
@@ -0,0 +1,15 @@
+--- Select Owners whose names include Ali
+
+SELECT *
+FROM `products`
+WHERE `product_Owner` LIKE '%Ali%';
+
+
+
+--- Select Owners whose names include Mohammad
+
+
+SELECT *
+FROM `products`
+WHERE `product_Owner` LIKE '%Mohammad%';
+
